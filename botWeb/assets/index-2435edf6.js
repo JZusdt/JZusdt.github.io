ef916dde9092bf2b9e782699a127678e5ec1425c
JZusdt <153887786+JZusdt@users.noreply.github.com>
2025-09-13 09:24:45 +0800

    Update index-2435edf6.js

--- botWeb/assets/index-2435edf6.js
+++ botWeb/assets/index-2435edf6.js
@@ -422,7 +422,7 @@ const I = e => (T("data-v-10d5ad81"), e = e(), U(), e),
                                 y = o.round(c - m, 2),
                                 v = o.round(p - x, 2),
                                 g = {};
-                            return g.insBaseCount = c, g.insCurrenyCount = p, g.outBaseCount = m, g.outCurrencyCount = x, g.leftBaseCount = y, g.leftCurrencyCount = v, g.remark = u[0].remark && u[0].remark.trim() !== "" ? u[0].remark : "无", g
+                            return g.insBaseCount = c, g.insCurrenyCount = p, g.outBaseCount = m, g.outCurrencyCount = x, g.leftBaseCount = y, g.leftCurrencyCount = v, g.remark = n && n.trim() !== "" ? n : "无", g
                         })),
                         l = {};
                     return l.group_content = n, l.group_len = t, l
